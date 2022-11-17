25f5366dbe2c120ab574afa9db5c5c7b587529d6
_alex_mga <alexalex78@ymail.com>
2022-11-17 13:25:59 +0100

    Create Utils.java

---
+++ .java/Utils.java
@@ -0,0 +1,32 @@
+package com.example.dotomorrow;
+
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.view.WindowMetrics;
+
+public class Utils {
+
+    public Utils() {
+    }
+
+
+    public void SortTasks_byName() {
+
+    }
+    public void SortTasks_byName_reverse() {
+
+    }
+    public void SortTasks_byDate() {
+
+    }
+    public void SortTasks_byDate_reverse() {
+
+    }
+    public void SortTasks_byLevel() {
+
+    }
+    public void SortTasks_byCategories() {
+
+    }
+}
